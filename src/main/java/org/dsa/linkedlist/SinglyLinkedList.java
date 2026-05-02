2182b74ea8e87e5f18dd6b316039a175eb7ad100
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-05-02 18:43:12 +0530

    Added traversal method

--- src/main/java/org/dsa/linkedlist/SinglyLinkedList.java
+++ src/main/java/org/dsa/linkedlist/SinglyLinkedList.java
@@ -2,5 +2,22 @@ package org.dsa.linkedlist;
 
 public class SinglyLinkedList {
 
+    private Node head;
+
+    public void traversal() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node curr = head;
+
+        while (curr != null) {
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+
+        System.out.println("null");
+    }
 
 }
