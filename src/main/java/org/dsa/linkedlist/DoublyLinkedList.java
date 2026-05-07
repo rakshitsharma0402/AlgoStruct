2ce02bb5ef5ebaeb4fa88b679bcde12d7ec53820
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-05-07 22:30:10 +0530

    Doubly linked list funtions

--- src/main/java/org/dsa/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/dsa/linkedlist/DoublyLinkedList.java
@@ -6,6 +6,32 @@ public class DoublyLinkedList {
     Node tail;
 
     /**
+     * Check if list is empty
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * Return size of linked list
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public int size() {
+        int count = 0;
+        Node curr = head;
+
+        while (curr != null) {
+            count++;
+            curr = curr.next;
+        }
+
+        return count;
+    }
+
+    /**
      * Traverse Forward
      * TC: O(n)
      * SC: O(1)
@@ -71,5 +97,41 @@ public class DoublyLinkedList {
         tail.next = newNode;
         newNode.prev = tail;
         tail = newNode;
+    }
+
+    /**
+     * Insert at specific position
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public void insertAtPosition(int val, int pos) {
+        int n = size();
+
+        if (pos < 0 || pos > n) {
+            throw new IndexOutOfBoundsException("Invalid position: " + pos);
+        }
+
+        if (pos == 0) {
+            insertAtBeginning(val);
+            return;
+        }
+
+        if (pos == n) {
+            insertAtEnd(val);
+            return;
+        }
+
+        Node newNode = new Node(val);
+        Node curr = head;
+
+        for (int i = 0; i < pos - 1; i++) {
+            curr = curr.next;
+        }
+
+        newNode.next = curr.next;
+        newNode.prev = curr;
+
+        curr.next.prev = newNode;
+        curr.next = newNode;
     }
 }
